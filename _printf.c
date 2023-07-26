e71242536609bbf89c8eb63c4fcfb224127d898c
Blakstorm <riz.essaib@gmail.com>
2023-07-26 16:29:36 -0700

    f

--- _printf.c
+++ _printf.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
 void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
