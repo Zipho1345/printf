3d1c4e446dfcdb43198052fba88d280191350e6c
Blakstorm <riz.essaib@gmail.com>
2023-07-28 14:31:38 -0700

    try to fix the main function

--- main.h
+++ main.h
@@ -109,7 +109,7 @@ char buffer[],
 
 /****************** UTILS ******************/
 int is_printable(char);
-int append_hexa_code(char, char[],int);
+int append_hexa_code(char, char[], int);
 int is_digit(char);
 
 long int convert_size_number(long int num, int size);
